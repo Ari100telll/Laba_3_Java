1772a4e52e61f2a528b876f5dde4deecd2de5a3b
Nikita Horb <masternik007@gmail.com>
2020-02-27 13:47:43 +0200

    add stringReader, change Writer and Reader logic

--- src/main/java/ua/lviv/iot/childrenevents/manager/EventOptionsReader.java
+++ src/main/java/ua/lviv/iot/childrenevents/manager/EventOptionsReader.java
@@ -1,26 +1,23 @@
 package ua.lviv.iot.childrenevents.manager;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 
 public class EventOptionsReader {
  
-	public String readFromFile(String fileName) {
-		File file = new File(fileName);
-		String returnString = "";
-		try (Reader reader = new FileReader(file, StandardCharsets.UTF_8);) {
+	Reader reader;
+
+	public void setReader(Reader reader) {
+		this.reader = reader;
+	}
+
+	public String readFromFile() throws IOException {
+			String returnString = "";
 			int oneCharInInt = reader.read();
 			while (oneCharInInt != -1) {
 				returnString = returnString + (char) oneCharInInt;
 				oneCharInInt = reader.read();
 			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		return returnString;
 	}
 
--- src/main/java/ua/lviv/iot/childrenevents/manager/EventOptionsWriter.java
+++ src/main/java/ua/lviv/iot/childrenevents/manager/EventOptionsWriter.java
@@ -1,29 +1,36 @@
 package ua.lviv.iot.childrenevents.manager;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import ua.lviv.iot.childrenevents.model.ChildrenEventsOption;
 
 public class EventOptionsWriter {
 
-	public void writeToFile(List<ChildrenEventsOption> childrenEventsOptions, String fileName) {
-		File file = new File(fileName);
-		try (Writer textWriter = new FileWriter(file, StandardCharsets.UTF_8);) {
-			writeLine(textWriter, childrenEventsOptions.get(0).getHeaders());
-			for (ChildrenEventsOption childrenEventsOption : childrenEventsOptions) {
-				writeLine(textWriter, childrenEventsOption.toCSV());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	Writer textWriter;
+
+	public Writer getWriter() {
+		return textWriter;
+	}
+
+	public void setWriter(Writer textWriter) {
+		this.textWriter = textWriter;
+	}
+
+	public void writeToFile(List<ChildrenEventsOption> childrenEventsOptions) throws IOException {
+		writeLine(textWriter, childrenEventsOptions.get(0).getHeaders());
+		for (ChildrenEventsOption childrenEventsOption : childrenEventsOptions) {
+			writeLine(textWriter, childrenEventsOption.toCSV());
 		}
+		textWriter.flush();
 	}
 
 	public void writeLine(Writer writer, String string) throws IOException {
 		writer.write(string + "\r\n");
 	}
 
+	@Override
+	public String toString() {
+		return textWriter.toString();
+	}
 }
